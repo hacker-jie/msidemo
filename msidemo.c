798fce44ca935c144ae01509620cecbda8cd8177
Jie Deng <jie.deng@intel.com>
2019-05-31 16:22:28 +0800

    init work version

--- msidemo.c
+++ msidemo.c
@@ -7,7 +7,7 @@
 #define VENDOR_ID	0x8086	// Intel Corporation
 #define DEVICE_ID	0x156f	// E1000_DEV_ID_PCH_SPT_I219_LM
 #define irqID		0x04	// temporary -- an unused IRQ
-#define intID		0x7F //0x49	// temporary -- IOAPIC mapped
+#define intID		0x49	// temporary -- IOAPIC mapped
 
 
 enum {
@@ -25,17 +25,56 @@ void		*io;
 wait_queue_head_t  my_wq;
 int		irqcount = 0;
 u32		msi_cap[4];
+int 		irq;
+
+static void print_msi_info(struct pci_dev  *devp)
+{
+        static int i = 0;
+        u32 msi_data[4];
+
+        if (devp == NULL)
+                return;
+
+        pci_read_config_dword(devp, 0xD0, &msi_data[0]);
+        pci_read_config_dword(devp, 0xD4, &msi_data[1]);
+        pci_read_config_dword(devp, 0xD8, &msi_data[2]);
+        pci_read_config_dword(devp, 0xDC, &msi_data[3]);
+
+        printk("%d. msi ctrl=%x\n", ++i, msi_data[0]);
+        printk("%d. msi addr=%x\n", i,   msi_data[1]);
+        printk("%d. msi data=%x\n", i,   msi_data[3]);
+}
+
+static void seq_print_msi_info(struct pci_dev  *devp, struct seq_file *m)
+{
+        static int i = 0;
+        u32 msi_data[4];
+
+        if (devp == NULL)
+                return;
+
+        pci_read_config_dword(devp, 0xD0, &msi_data[0]);
+        pci_read_config_dword(devp, 0xD4, &msi_data[1]);
+        pci_read_config_dword(devp, 0xD8, &msi_data[2]);
+        pci_read_config_dword(devp, 0xDC, &msi_data[3]);
+
+        seq_printf(m, "%d. msi ctrl=%x\n", ++i, msi_data[0]);
+        seq_printf(m, "%d. msi addr=%x\n", i,   msi_data[1]);
+        seq_printf(m, "%d. msi data=%x\n", i,   msi_data[3]);
+}
 
 static int msidemo_show(struct seq_file *m, void *v)
 {
 	int oldcount;
 
 	seq_printf(m, "proir irqcount = %d\n", irqcount);
-	seq_printf(m, "now triggering another MSI\n");
+	seq_printf(m, "triggering another MSI\n");
 
 	oldcount = irqcount;
         iowrite32(1<<4, io + E1000_ICS);
         wait_event_interruptible(my_wq, irqcount != oldcount);
+
+	seq_print_msi_info(devp, m);
 
 	return 0;
 }
@@ -52,7 +91,7 @@ static const struct file_operations msidemo_ops = {
 	.release = single_release,
 };
 
-irqreturn_t my_isr( int irq, void *dev_id )
+irqreturn_t my_isr(int irq, void *dev_id)
 {
 	int interrupt_cause = ioread32(io + E1000_ICR);
 	if (interrupt_cause == 0)
@@ -68,10 +107,11 @@ irqreturn_t my_isr( int irq, void *dev_id )
 	return IRQ_HANDLED;
 }
 
-static int __init msidemo_init( void )
+static int __init msidemo_init(void)
 {
-	u16	pci_cmd;
-	u32	msi_ctrl, msi_addr, msi_data; 
+	u16 pci_cmd;
+	u32 msi_ctrl, msi_addr, msi_data;
+	int ret; 
 
 	printk("<1>\nInstalling \'%s\' module\n", modname);
 
@@ -79,6 +119,23 @@ static int __init msidemo_init( void )
 	devp = pci_get_device(VENDOR_ID, DEVICE_ID, NULL);
 	if (!devp)
 		return -ENODEV;
+
+	print_msi_info(devp);
+
+        // save contents the I219's MSI Capability Register Set 
+        pci_read_config_dword(devp, 0xD0, &msi_cap[0]);
+        pci_read_config_dword(devp, 0xD4, &msi_cap[1]);
+        pci_read_config_dword(devp, 0xD8, &msi_cap[2]);
+        pci_read_config_dword(devp, 0xDC, &msi_cap[3]);
+
+	//enable pci device
+	ret = pcim_enable_device(devp);
+	if (ret < 0) {
+        	printk("enable pci device fail, ret=%d\n", ret);
+                return ret;
+        }
+	
+	print_msi_info(devp);
 
 	// map controller's i/o-memory into kernel's address-space
 	mmio_base = pci_resource_start(devp, 0);
@@ -92,16 +149,6 @@ static int __init msidemo_init( void )
 	pci_cmd |= (1 << 2);			// enable Bus Master
 	pci_write_config_word(devp, 4, pci_cmd);
 
-	// save contents the I219's MSI Capability Register Set 
-	pci_read_config_dword(devp, 0xD0, &msi_cap[0]);	
-	pci_read_config_dword(devp, 0xD4, &msi_cap[1]);	
-	pci_read_config_dword(devp, 0xD8, &msi_cap[2]);	
-	pci_read_config_dword(devp, 0xDC, &msi_cap[3]);	
-
-	printk("msi data=%x\n", msi_cap[3]);
-	printk("msi addr=%x\n", msi_cap[1]);
-	printk("msi_addr=%x\n", msi_cap[0]);
-
 	// configure the I219 to generate Message Signaled Interrupts
 	msi_data = 0x4000 | intID;
 	msi_addr = 0xFEE002f8;
@@ -110,11 +157,22 @@ static int __init msidemo_init( void )
 	pci_write_config_dword(devp, 0xD4, msi_addr);
 	pci_write_config_dword(devp, 0xD0, msi_ctrl);
 
+        print_msi_info(devp);
+
 	// initialize our module's wait-queue 
 	init_waitqueue_head(&my_wq);
 
+	// Get irq
+	ret = pci_alloc_irq_vectors(devp, 1, 1, PCI_IRQ_MSI);
+	if (ret < 0) {
+		printk("alloc IRQ_MSI fail, ret=%d\n", ret);
+		return ret;
+	}
+	irq = pci_irq_vector(devp, 0);
+	printk("irq = %d\n", irq);
+
 	// install interrupt-handler and enable I219 interrupts
-	if (request_irq(irqID, my_isr, IRQF_SHARED, modname, &modname ) < 0) {
+	if (request_irq(irq, my_isr, IRQF_SHARED, modname, &modname ) < 0) {
 		iounmap(io);
 		return -EBUSY;
 	}
@@ -122,6 +180,9 @@ static int __init msidemo_init( void )
 
 	// create this module's pseudo-file
 	proc_create(modname, 0, NULL, &msidemo_ops);
+
+	printk("End of msidemo\n");
+
 	return	0;  //SUCCESS
 }
 
@@ -133,7 +194,8 @@ static void __exit msidemo_exit(void)
 
 	// disable I219 interrupts and remove interrupt-handler
 	iowrite32(0xFFFFFFFF, io + E1000_IMC);
-	free_irq(irqID, modname);
+	free_irq(irq, modname);
+	pci_free_irq_vectors(devp);	
 
 	// restore original values to the MSI Capability Register Set
 	pci_write_config_dword(devp, 0xD0, msi_cap[0]);
@@ -147,6 +209,6 @@ static void __exit msidemo_exit(void)
 	printk("<2>Removing \'%s\' module\n", modname);
 }
 
-module_init( msidemo_init );
-module_exit( msidemo_exit );
+module_init(msidemo_init);
+module_exit(msidemo_exit);
 MODULE_LICENSE("GPL");
